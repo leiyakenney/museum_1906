dd9b63e9b3a83fc0e9d7c42c5e3fc9da4734c102
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-17 14:19:06 -0600

    Complete Iteration 1

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -13,4 +13,16 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, @bob
   end
 
+  def test_attributes
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+    assert_equal [], @bob.interests
+  end
+
+  def test_add_interests
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    assert ["Dead Sea Scrolls", "Gems and Minerals"], @bob.interests
+  end
+
 end
