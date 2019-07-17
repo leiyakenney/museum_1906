2197e6f07e15ce611f58309d91d20a397e228950
Leiya Kenney <leiya.kenney@gmail.com>
2019-07-17 14:26:48 -0600

    Complete I2 through adding exhibits to museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,14 +1,33 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/exhibit'
 require './lib/patron'
+require './lib/museum'
 
-class PatronTest < Minitest::Test
+class MuseumTest < Minitest::Test
 
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+    @bob = Patron.new("Bob", 20)
   end
 
   def test_it_exists
     assert_instance_of Museum, @dmns
+  end
+
+  def test_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 end
